b14464d2b83e691e7ff429f7bd28c852842ab5f3
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2024-02-16 17:00:16 -0300

    first commit

---
+++ CrudProduto/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿let table = new DataTable('#table', {
+    language: {
+        "decimal": "",
+        "emptyTable": "Sem dados disponíveis na tabela",
+        "info": "Mostrando página _PAGE_ até _PAGES_",
+        "infoEmpty": "Mostrando 0 a 0 de 0 entradas",
+        "infoFiltered": "(filtrado do total de _MAX_ entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Exibir registros _MENU_ por página",
+        "loadingRecords": "Carregando...",
+        "processing": "",
+        "search": "Procurar:",
+        "zeroRecords": "Nenhum registro correspondente encontrado",
+        "aria": {
+            "orderable": "Ordenar por esta coluna",
+            "orderableReverse": "Ordem inversa desta coluna"
+        }
+    }
+});
+
